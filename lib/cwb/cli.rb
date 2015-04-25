bb3b61e4b27d38c06f09ce9f993d0cefad8bbed2
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-04-25 17:34:02 +0200

    Remove duplicated error logging, let exception raise

--- lib/cwb/cli.rb
+++ lib/cwb/cli.rb
@@ -7,18 +7,12 @@ module Cwb
     option :aliases => :e
     def execute(path)
       Cwb::ParserFactory.build(path).execute
-    rescue => error
-      puts "Error: #{error.message}"
-      raise error
     end
     
     desc "validates BENCHMARK_FILE|BENCHMARK_DIRECTORY", "performs basic validation of the class files and directory structure."
     option :aliases => :v
     def validate(path)
       Cwb::ParserFactory.build(path).validate
-    rescue => error
-      puts "Error: #{error.message}"
-      raise error
     end
   end
 end
